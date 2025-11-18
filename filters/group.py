ec907bbc5dd780065e4a63a9f863f7ca662224f3
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2025-11-18 05:53:16 -0500

    Update group.py

--- filters/group.py
+++ filters/group.py
@@ -8,5 +8,7 @@ class IsGroup(BoundFilter):
         return message.chat.type in [types.ChatType.GROUP, types.ChatType.SUPERGROUP]
 
 class IsPDFGroup(BoundFilter):
-    async def check_group(self,types.Message):
-        return message.chat.id in [-1003341826791,"-1003341826791"]
+    async def check(self, message: types.Message):
+        allowed_groups = [-1003341826791, -1001234567890]
+        return message.chat.id in allowed_groups
+
