99e4df00242f74529effae0e186bddfa29bf5719
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2025-11-18 01:32:40 -0500

    Update group.py

--- filters/group.py
+++ filters/group.py
@@ -6,3 +6,7 @@ from aiogram import types
 class IsGroup(BoundFilter):
     async def check(self, message: types.Message):
         return message.chat.type in [types.ChatType.GROUP, types.ChatType.SUPERGROUP]
+
+class IsPDFGroup(BoundFilter):
+    async def check_group(self,types.Message):
+        return message.chat.id in [-1003341826791,"-1003341826791"]
